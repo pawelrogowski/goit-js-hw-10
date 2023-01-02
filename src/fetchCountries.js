94ab465376a13c391e9311fc073a440e3872a3c8
Pawel Rogowski <pawel.rogowski.praca@gmail.com>
2023-01-02 20:01:04 +0100

    Revised the code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,18 @@
-function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v2/name/${name}?fields=capital,population,flags,languages`)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(`Request failed with status code ${response.status}`);
-      }
-      return response;
-    })
-    .then(response => response.json());
+/*
+This module exports a function that does the following:
+
+- Accepts a name parameter
+- Makes a GET request to the restcountries API for a list of countries with a name matching the name parameter
+- If the request is not successful (i.e. response is not "ok"), throws an error with the response status as the message
+- If the request is successful, returns the response in JSON format
+*/
+
+const fetchCountryFields = 'name,capital,population,flags,languages';
+export function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v2/name/${name}?fields=${fetchCountryFields}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,58 +1,93 @@
+/*
+This script does the following:
+
+- Imports the necessary libraries and modules
+- Selects DOM elements for the search input, search results list, and search results info
+- Defines a function to clear the country list and info elements
+- Defines a function to create list items for each country in the array
+- Defines a function to create a list item element for a given name and value
+- Defines a function to create the country info elements
+- Defines a debounced function to retrieve and display country data based on user input
+- Adds an input event listener to the search input element to execute the populateCountryHtml function
+
+*/
+
 import './css/styles.css';
 import { debounce } from 'lodash';
 import { Notify } from 'notiflix';
-
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
-
-const fetchCountryFields = 'name, capital,population,flags,languages';
-function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v2/name/${name}?fields=${fetchCountryFields}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
 
 const searchInput = document.querySelector('#search-box');
 const searchResultsList = document.querySelector('.country-list');
 const searchResultsInfo = document.querySelector('.country-info');
 
-const clearCountryList = () => (searchResultsList.innerHTML = '');
+const clearCountryElements = () => {
+  searchResultsList.innerHTML = '';
+  searchResultsInfo.innerHTML = '';
+};
 
-const countryFlagWidth = '100px';
+const createCountryListItems = countries => {
+  const listItems = countries.map(country => {
+    const listItem = document.createElement('li');
+    listItem.className = 'country-list__item';
+    listItem.innerHTML = `
+      <img class="country-list__image" src="${country.flags.svg}" alt="Flag of ${country.name}">
+      <span>${country.name}</span>
+    `;
+    return listItem;
+  });
+  return listItems;
+};
+
+const createCountryInfoListItem = (name, value) => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<strong>${name}:</strong> ${value}`;
+  return listItem;
+};
+
+const createCountryInfo = country => {
+  const header = document.createElement('div');
+  header.className = 'country-info__header';
+  header.innerHTML = `
+    <img class="country-info__image" src="${country.flags.svg}" alt="Flag of ${country.name}">
+    <span>${country.name}</span>
+  `;
+
+  const list = document.createElement('ul');
+  list.className = 'country-info__list';
+
+  const capitalItem = createCountryInfoListItem('Capital', country.capital);
+  const populationItem = createCountryInfoListItem('Population', country.population);
+  const languagesItem = createCountryInfoListItem(
+    'Languages',
+    country.languages.map(language => language.name).join(', ')
+  );
+
+  list.append(capitalItem, populationItem, languagesItem);
+  return [header, list];
+};
 
 const populateCountryHtml = debounce(event => {
   const searchString = event.target.value.trim();
   if (!searchString) {
-    clearCountryList();
+    clearCountryElements();
     return;
   }
 
-  clearCountryList();
+  clearCountryElements();
 
   fetchCountries(event.target.value)
     .then(data => {
       if (data.length > 10) {
         Notify.info('Too many matches found. Please enter a more specific name.');
-      } else if (data.length > 2) {
-        clearCountryList();
-
-        const listItems = [];
-        for (const country of data) {
-          console.log(country);
-          const listItem = document.createElement('li');
-          listItem.innerHTML = `
-            <figure>
-              <figcaption>${country.name}</figcaption>
-              <img src="${country.flags.svg}" alt="Flag of ${country.name}" width=${countryFlagWidth}>
-            </figure>
-          `;
-          listItems.push(listItem);
-        }
-        searchResultsList.append(...listItems); // not supported in IE
+      } else if (data.length > 1) {
+        clearCountryElements();
+        searchResultsList.append(...createCountryListItems(data));
       } else {
-        console.log(data);
+        const [header, list] = createCountryInfo(data[0]);
+        searchResultsInfo.innerHTML = '';
+        searchResultsInfo.append(header, list);
       }
     })
     .catch(error => {
